50d15917c90eca7164a818392a43aec85203120d
s2348830-commits <s2348830@gmail.com>
2026-04-10 05:54:32 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const MONGO_URI = "mongodb+srv://nakimusispec_db_user:20060403@cluster0.tueqe88.
 const CLIENT_ID = "1491471551512449255"; 
 const CLIENT_SECRET = "h2HAw_NEwR5TirXfELAsFM_ohg2XR_Ed"; 
 // あなたのRenderのURL（https://〜.onrender.com）に書き換えてください
-const REDIRECT_URI = "https://あなたのアプリ名.onrender.com/api/callback";
+const REDIRECT_URI = "https://rpg-shop-web-abcd.onrender.com/api/callback";
 // ==========================================
 
 app.use(express.json()); 
